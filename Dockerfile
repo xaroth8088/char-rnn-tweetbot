611e3c0ce3e4d1b78c2c5931faaa55ac03b2d983
xaroth <xaroth@gmail.com>
2020-06-02 15:14:19 -0700

    Initial working version

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.7.7-buster
+WORKDIR /app
+
+# Grab and setup the NN repo
+RUN git clone https://github.com/xaroth8088/char-rnn.git .
+RUN git checkout 9b4910d
+RUN pip install -r requirements.txt
+RUN rm -rf data/shakespeare
+
+# Layer this repo on top and set it up
+COPY . .
+RUN pip install -r requirements.txt
---
+++ __main__.py
@@ -0,0 +1,26 @@
+import twitter
+import configparser
+import os
+
+
+def main():
+    # Get tweet text
+    with os.popen(
+        "python sample.py --data_dir data/checkpoint-to-tweet-from --temperature 0.25 --sample_len 1000 | sed '2q;d'"
+    ) as stream:
+        output = stream.read()
+
+    # Load config
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    api = twitter.Api(consumer_key=config['twitter_app']['consumer_key'],
+                      consumer_secret=config['twitter_app']['consumer_secret'],
+                      access_token_key=config['twitter_app']['access_token'],
+                      access_token_secret=config['twitter_app']['access_token_secret'])
+
+    api.PostUpdates(output, continuation=u'…')
+
+
+if __name__ == "__main__":
+    main()
