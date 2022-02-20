a32a649d1f1ee91706ec7c88d12c002688c39aa3
Geoffrey Benson <xaroth@gmail.com>
2022-02-19 22:44:06 -0800

    Switch to use tweepy, to match updated Twitter API requirements

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-import twitter
+import tweepy
 import configparser
 import os
 
@@ -6,7 +6,7 @@ import os
 def main():
     # Get tweet text
     with os.popen(
-        "python sample.py --data_dir data/checkpoint-to-tweet-from --temperature 0.25 --sample_len 1000 | sed -E 's/[\\r\\n]+/\\n/g' | sed '2q;d'"
+            "python sample.py --data_dir data/checkpoint-to-tweet-from --temperature 0.25 --sample_len 280 | sed -E 's/[\\r\\n]+/\\n/g' | sed '2q;d'"
     ) as stream:
         output = stream.read()
 
@@ -14,12 +14,14 @@ def main():
     config = configparser.ConfigParser()
     config.read('config.ini')
 
-    api = twitter.Api(consumer_key=config['twitter_app']['consumer_key'],
-                      consumer_secret=config['twitter_app']['consumer_secret'],
-                      access_token_key=config['twitter_app']['access_token'],
-                      access_token_secret=config['twitter_app']['access_token_secret'])
+    api = tweepy.Client(
+        consumer_key=config['twitter_app']['consumer_key'],
+        consumer_secret=config['twitter_app']['consumer_secret'],
+        access_token=config['twitter_app']['access_token'],
+        access_token_secret=config['twitter_app']['access_token_secret']
+    )
 
-    api.PostUpdates(output, continuation=u'…')
+    api.create_tweet(text=output)
 
 
 if __name__ == "__main__":
